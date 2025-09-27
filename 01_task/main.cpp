b86d4bc7d66e439738ee7354c26ad8d4b8b913c1
Evgenii Blinov <scythian89@gmail.com>
2025-09-27 22:36:58 +0300

    init commit

---
+++ 01_task/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+int main() {
+    std::cout << "Speedometer" << std::endl;
+
+    float speed_value = 0.0;
+    char speed_str[5];
+    float speed_delta;
+
+    do {
+        std::cout << "Speed delta:";
+        std::cin >> speed_delta;
+        speed_value += speed_delta;
+        if (speed_value < 0.0) {
+            speed_value = 0.0;
+        }
+        if (speed_value > 150.0) {
+            speed_value = 150.0;
+        }
+
+        std::sprintf(speed_str, " %.1f", speed_value);
+        std::cout << "Speed:" << speed_str << std::endl;
+
+    } while (speed_value > 0);
+
+}
